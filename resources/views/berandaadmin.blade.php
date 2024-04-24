f114bc37cc572494736e97d33c4ddfc6a803e211
lovyy9 <putriarensya05@gmail.com>
2024-04-24 11:00:55 +0700

    menambahkan halaman admin

---
+++ resources/views/berandaadmin.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Beranda Admin</title>
+<link rel="stylesheet" href="berandaadmin.css">
+</head>
+<body>
+    <div class="navbar">
+        <img src="asset/img.beranda/Logo.png" alt="Logo" class="logo">
+        <a href="#" class="text">Log Out</a>
+    </div>
+    <div class="container">
+        <div>
+            <div class="small-box">
+                <img src="asset/img.beranda/BackgroundEraser_20240328_215034651.png" alt="Icon 1">
+            </div>
+            <p class="text" style="text-align: center;">Lihat Feedback</p>
+        </div>
+        <div>
+            <div class="small-box">
+                <img src="asset/img.beranda/BackgroundEraser_20240328_215059491.png" alt="Icon 2">
+            </div>
+            <p class="text" style="text-align: center;">Edit Map</p>
+        </div>
+    </div>
+</body>
+</html>
