d82fe261d70e966365799b84698f8990a25cafb9
lovyy9 <putriarensya05@gmail.com>
2024-06-10 05:57:39 +0700

    Beranda admin

--- resources/views/maps.blade.php
+++ resources/views/maps.blade.php
@@ -23,6 +23,8 @@
     <!-- font end -->
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="css/home.css" />
+    
+    <link rel="stylesheet" href="css/rektempat.css">
     <style>
         body * {
             padding: 0;
@@ -51,6 +53,57 @@
         </div>
     </section>
     {{-- navbar end --}}
+
+    <section class="absolute top-10 rigth-5 z-[9999] ">
+    <div class="search-container">
+            <input type="text" class="input-search" placeholder="cari lokasi wisata" id="searchInput">
+            <div class="popup">
+                <div class="popup-content">
+                    <span class="close-popup" onclick="closePopup()">&times;</span>
+                    <h3 class="recommendation-header">Recommendation</h3>
+                    <div class="gambar-info">
+                        <div class="gambar-info-item">
+                            <img src="asset/rekomendasi/telaga.jpg" alt="Telaga Sarangan">
+                            <p class="gambar-caption">Telaga Sarangan</p>
+                        </div>
+                        <div class="gambar-info-item">
+                            <img src="asset/rekomendasi/gunung lawu.jpg" alt="Gunung Lawu">
+                            <p class="gambar-caption">Gunung Lawu</p>
+                        </div>
+                        <div class="gambar-info-item">
+                            <img src="asset/rekomendasi/taman wisata genilangit.jpg" alt="Taman Wisata Genilangit">
+                            <p class="gambar-caption">Taman Wisata Genilangit</p>
+                        </div>
+                        <div class="gambar-info-item">
+                            <img src="asset/rekomendasi/Air trjun tirtosari.jpg" alt="Air Terjun Tirtosari">
+                            <p class="gambar-caption">Air Terjun Tirtosari</p>
+                        </div>
+                        <div class="gambar-caption">
+                            <h4>Top Search</h4>
+                            <p class="top-search-paragraph"><a href="#">Cemoro Sewu</a></p>
+                            <p class="top-search-paragraph"><a href="#">Mojosemi Forest Park</a></p>
+                            <p class="top-search-paragraph"><a href="#">Mbah Djoe Resort</a></p>
+                            <p class="top-search-paragraph"><a href="#">Kebun Refugia Magetan</a></p>
+                            <p class="top-search-paragraph"><a href="#">Telaga Sarangan</a></p>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="filter-container">
+                <div class="kotak-baru">
+                    <h3>Filter</h3>
+                    <p>berdasar jenis</p>
+                    <p>berdasar wilayah</p>
+                </div>
+                <div class="kotak-kecil-container">
+                    <button class="kotak-kecil">+</button>
+                    <button class="kotak-kecil">-</button>
+                    <button class="kotak-kecil"><i class="fas fa-map-marker-alt"></i></button>
+                </div>
+            </div>
+        </div>
+    </section>
 
     {{-- Maps Start --}}
     <div id="map"></div>
